c8367cd0d2bc17b3c5765bfbce2b41503c39b273
shaker-project <65733442+shaker-project@users.noreply.github.com>
2021-06-10 14:03:49 -0300

    Add a simple project

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import java.util.Random;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void definitelyNotFlaky()
+    {
+        assertTrue( false );
+    }
+
+    @Test
+    public void Test2()
+    {
+        Random random = new Random();
+        assertTrue( random.nextDouble() > 0.3 );
+    }
+}
